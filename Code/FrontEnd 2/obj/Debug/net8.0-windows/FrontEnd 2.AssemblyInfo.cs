1f2346f8a0a4b6b19f1d302056028d2317102722
Vadym Kaliuzhnyi <KaliuzhnyiV190154@sus.htl-grieskirchen.at>
2024-01-22 13:00:00 +0100

    Chilling

--- Code/FrontEnd 2/obj/Debug/net8.0-windows/FrontEnd 2.AssemblyInfo.cs
+++ Code/FrontEnd 2/obj/Debug/net8.0-windows/FrontEnd 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fcbdd130b4de3847bddf173eedfa64698ea50a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
