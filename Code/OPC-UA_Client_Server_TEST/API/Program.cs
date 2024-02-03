04214593100ad9c3d02297accbcaa1e2c3cfedad
HunterPat <KeiserP190271@sus.htl-grieskirchen.at>
2024-02-03 17:44:41 +0100

    Services & Maps

--- Code/OPC-UA_Client_Server_TEST/API/Program.cs
+++ Code/OPC-UA_Client_Server_TEST/API/Program.cs
@@ -1,3 +1,5 @@
+using API.Maps;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using OPC_UA_Client;
 
@@ -7,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<MachineServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,75 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//Server
-int gesamtTubenAnzZiel = 0;
-string server1URL = "opc.tcp://localhost:4840/";
-string server2URL = "opc.tcp://localhost:4841/";
-MyOPCServer1 server1 = new MyOPCServer1(server1URL);
-MyOPCServer2 server2 = new MyOPCServer2(server2URL);
-server1.StartServer();
-server2.StartServer();
-//Client
-MyOPCClient client = new MyOPCClient(server1URL);
-client.EstablishConnection();
-
-app.MapGet("/{serverID}/TAA1", (int serverID) =>
-{
-    if(serverID == 1)
-    {
-        if(client.serverURL != server1URL)
-        {
-            client.Disconnect();
-            client = new MyOPCClient(server1URL);
-            client.EstablishConnection();
-        }
-        return client.ReadDataFromTAA1().Value;
-
-    }else if(serverID == 2)
-    {
-        if (client.serverURL != server2URL)
-        {
-            client.Disconnect();
-            client = new MyOPCClient(server2URL);
-            client.EstablishConnection();
-        }
-        return client.ReadDataFromTAA1().Value;
-    }
-    return null;
-});
-app.MapGet("/{serverID}/TAA2", (int serverID) =>
-{
-    if (serverID == 1)
-    {
-        if (client.serverURL != server1URL)
-        {
-            client.Disconnect();
-            client = new MyOPCClient(server1URL);
-            client.EstablishConnection();
-        }
-        return client.ReadDataFromTAA2().Value;
-
-    }
-    else if (serverID == 2)
-    {
-        if (client.serverURL != server2URL)
-        {
-            client.Disconnect();
-            client = new MyOPCClient(server2URL);
-            client.EstablishConnection();
-        }
-        return client.ReadDataFromTAA2().Value;
-    }
-    return null;
-});
-var gesamtTubenAnzGroup = app.MapGroup("/gesamttubenanz");
-gesamtTubenAnzGroup.MapGet("", () => gesamtTubenAnzZiel);
-
-gesamtTubenAnzGroup.MapPost("", ([FromBody] int value) =>
-{
-    gesamtTubenAnzZiel = value;
-    return value;
-});
+app.MapMachineData();
 app.Run();
 public class GesamtTubenAnzZiel
 {
--- Code/OPC-UA_Client_Server_TEST/OPC-UA_Client/obj/Debug/net7.0/OPC-UA_Client.AssemblyInfo.cs
+++ Code/OPC-UA_Client_Server_TEST/OPC-UA_Client/obj/Debug/net7.0/OPC-UA_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OPC-UA_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad12952b9de57daac8517c7b3fac3238b8b1ba26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2346f8a0a4b6b19f1d302056028d2317102722")]
 [assembly: System.Reflection.AssemblyProductAttribute("OPC-UA_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OPC-UA_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
