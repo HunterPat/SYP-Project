9e3de84bcd73c033a7a01ba1fb42bb91558f362c
Vadym Kaliuzhnyi <KaliuzhnyiV190154@sus.htl-grieskirchen.at>
2024-02-03 17:09:07 +0100

    Added 2 Frontend Projects

---
+++ Code/ProdVisOverviewFrontend/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdVisOverviewFrontend
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string _dateText;
+
+        public string DateText
+        {
+            get { return _dateText; }
+            set
+            {
+                if (_dateText != value)
+                {
+                    _dateText = value;
+                    OnPropertyChanged(nameof(DateText));
+                }
+            }
+        }
+
+        private int productionGoal_1;
+
+        public int ProductionGoal_1
+        {
+            get { return productionGoal_1; }
+            set
+            {
+                if (productionGoal_1 != value)
+                {
+                    productionGoal_1 = value;
+                    OnPropertyChanged(nameof(ProductionGoal_1));
+                }
+            }
+        }
+
+        private int currentAmount_1;
+
+        public int CurrentAmount_1
+        {
+            get { return currentAmount_1; }
+            set
+            {
+                if (currentAmount_1 != value)
+                {
+                    currentAmount_1 = value;
+                    OnPropertyChanged(nameof(CurrentAmount_1));
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Code/ProdVisOverviewFrontend/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ProdVisOverviewFrontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel;
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
+
+            // Set an initial value for DateText
+            viewModel.DateText = "Initial Date";
+
+            // Set up the timer to update DateText every second
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            viewModel.ProductionGoal_1 = 1000;
+            viewModel.CurrentAmount_1 = 345;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update DateText with the current DateTime.Now value
+            viewModel.DateText = DateTime.Now.ToString("dddd, dd.MM.yyyy - HH:mm:ss");
+        }
+    }
+}
