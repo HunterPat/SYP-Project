cead4bd62157c81bb2bf12ac5ed8d7544e26d329
kaliuzhnyiv190154 <KaliuzhnyiV190154@sus.htl-grieskirchen.at>
2024-02-04 22:55:05 +0100

    retry

---
+++ Code/ProdVisAdminFrontend/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ProdVisAdminFrontend.ViewModels
+{
+    class OverviewViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _dateText;
+        private DispatcherTimer timeTimer;
+
+        public string DateText
+        {
+            get { return _dateText; }
+            set
+            {
+                if (_dateText != value)
+                {
+                    _dateText = value;
+                    OnPropertyChanged(nameof(DateText));
+                }
+            }
+        }
+
+
+        public OverviewViewModel()
+        {
+            timeTimer = new DispatcherTimer();
+            _dateText = "Loading...";
+            timeTimer.Interval = TimeSpan.FromMilliseconds(500);
+            timeTimer.Tick += TimeTimer_Tick;
+            timeTimer.Start();
+        }
+
+        private void TimeTimer_Tick(object? sender, EventArgs e)
+        {
+            DateText = DateTime.Now.ToString("dddd, dd.MM.yyyy - HH:mm:ss");
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
