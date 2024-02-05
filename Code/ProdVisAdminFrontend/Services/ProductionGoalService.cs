60cd8a4889a5789ccb2d458a6a515ef7e08c29e8
kaliuzhnyiv190154 <KaliuzhnyiV190154@sus.htl-grieskirchen.at>
2024-02-05 20:59:38 +0100

    updated admin frontend

---
+++ Code/ProdVisAdminFrontend/Services/ProductionGoalService.cs
@@ -0,0 +1,48 @@
+﻿using Org.OpenAPITools.Api;
+using ProdVisAdminFrontend.SharedServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdVisAdminFrontend.Services
+{
+    class ProductionGoalService
+    {
+        private bool isRunning = true;
+        private readonly ISharedService productionGoal_1;
+        private readonly ISharedService productionGoal_2;
+        private APIApi api;
+        private const string baseUrl = "http://localhost:5501";
+
+        public ProductionGoalService(ISharedService productionGoal_1, ISharedService productionGoal_2)
+        {
+            api = new APIApi(baseUrl);
+            this.productionGoal_1 = productionGoal_1;
+            this.productionGoal_2 = productionGoal_2;
+        }
+
+        public void Start()
+        {
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    // Your background code
+                    Thread.Sleep(1000); // Example: Sleep for 1 second
+                    if (api == null) return;
+                    productionGoal_1.ProductionGoal = api.GesamttubenanzZielGet();
+                    productionGoal_2.ProductionGoal = api.GesamttubenanzZielGet();
+
+
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+    }
+}
---
+++ Code/ProdVisAdminFrontend/SharedServices/SharedProductionGoal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdVisAdminFrontend.SharedServices
+{
+    internal class SharedProductionGoal : ISharedService, INotifyPropertyChanged
+    {
+        private int productionGoal;
+
+        public int ProductionGoal
+        {
+            get { return productionGoal; }
+            set
+            {
+                if (productionGoal != value)
+                {
+                    productionGoal = value;
+                    OnPropertyChanged(nameof(productionGoal));
+                }
+            }
+        }
+
+        public SharedProductionGoal(int productionGoal)
+        {
+            ProductionGoal = productionGoal;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
